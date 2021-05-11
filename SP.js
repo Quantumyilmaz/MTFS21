c6b365377c6bcacc72a0cc8c0d4286273ef22448
Quantumyilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2021-05-12 00:07:30 +0200

    working on webpage

--- SP.js
+++ SP.js
@@ -1,9 +1,16 @@
+
 /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var sidenav = document.getElementById("sidenav");
 var topnav = document.getElementById("topnav");
-var main  = document.getElementById("main")
+var mains = document.getElementsByClassName("main");
+var main  = document.getElementById("main");
 var i;
+var parmak_payi = 40;
+main.style.marginLeft = (sidenav.offsetWidth+parmak_payi).toString() + "px";
+topnav.style.marginLeft = sidenav.offsetWidth.toString() + "px";
+topnav.style.width = (screen.width - sidenav.offsetWidth).toString() + "px";
+
 
 for (i = 0; i < dropdown.length; i++) {
     dropdown[i].addEventListener("click", function() {
@@ -21,9 +28,22 @@ for (i = 0; i < dropdown.length; i++) {
     });
 }
 
-var parmak_payi = 40 
-topnav.style.marginLeft = sidenav.offsetWidth.toString() + "px";
-topnav.style.width = (screen.width - sidenav.offsetWidth).toString() + "px";
-topnav.style.right = (main.innerWidth + parmak_payi).toString() + "px";
-main.style.marginLeft = (sidenav.offsetWidth+parmak_payi).toString() + "px";
-topnav.style.width = (screen.width - sidenav.offsetWidth).toString() + "px";
+document.getElementById("demo").innerHTML= main.style.padding;
+
+
+function myFunc(id){
+
+    for (i = 0; i < mains.length; i++) {
+        mains[i].style.display = "none"
+    }
+    main = document.getElementById("main_"+id);
+    
+    main.style.display="block";
+    main.style.top = "1000px";
+
+    main.style.width = (screen.width-sidenav.offsetWidth).toString() + "px";
+    main.style.height = (screen.height - topnav.offsetHeight).toString() + "px";
+    main.style.marginLeft = (sidenav.offsetWidth-8).toString() + "px";
+    main.style.marginTop = (topnav.offsetHeight).toString() + "px";
+}
+
