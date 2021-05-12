34d715f596d158d2a63bcda53121735c63ad36a3
Quantumyilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2021-05-12 19:22:18 +0200

    changed outlay of website

--- SP.js
+++ SP.js
@@ -6,10 +6,24 @@ var topnav = document.getElementById("topnav");
 var mains = document.getElementsByClassName("main");
 var main  = document.getElementById("main");
 var i;
+var j;
+main.style.width = (screen.width-sidenav.offsetWidth).toString() + "px";
+main.style.height = (screen.height - topnav.offsetHeight).toString() + "px";
 main.style.marginLeft = (sidenav.offsetWidth).toString() + "px";
+main.style.marginTop = (topnav.offsetHeight+100).toString() + "px";
 topnav.style.marginLeft = sidenav.offsetWidth.toString() + "px";
 topnav.style.width = 100*(1 - sidenav.offsetWidth/screen.width).toString() + "%";
 
+// document.getElementById("demo").innerHTML = document.getElementsByClassName("column")[1].getElementsByClassName("column_mini").length;
+var columns = document.getElementsByClassName("column");
+for (i = 0; i<columns.length; i++) {
+    columns[i].style.width = (100/columns.length)*(1 - sidenav.offsetWidth/screen.width).toString() + "%";
+    columns[i].style.marginLeft = (100/(columns.length+1))*(sidenav.offsetWidth/screen.width).toString() + "%";
+    var childrenz = columns[i].getElementsByClassName("column_mini");
+    for (j=0;j<childrenz.length;j++){
+        childrenz[j].style.width = (100/childrenz.length).toString() + "%";
+    }
+}
 
 for (i = 0; i < dropdown.length; i++) {
     dropdown[i].addEventListener("click", function() {
