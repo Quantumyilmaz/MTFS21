9aa42a944227e8fc6ae54fa623e79c6e06325f98
Quantumyilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2021-05-13 01:25:25 +0200

    Update MT.js

--- MT.js
+++ MT.js
@@ -1,9 +1,9 @@
 function resize()
 {
     var sidenav = document.getElementById("sidenav");
-    var topnav = document.getElementById("topnav");
+    // var topnav = document.getElementById("topnav");
     // var mains = document.getElementsByClassName("main");
-    var main  = document.getElementById("main");
+    // var main  = document.getElementById("main");
     var i;
     var j;
     // main.style.width = (screen.width-sidenav.offsetWidth).toString() + "px";
@@ -27,42 +27,33 @@ function resize()
 }
 
 function myFunc(id){
-
+    var main  = document.getElementById("main");
     main.style.display = "none";
     main = document.getElementById("main_"+id);
     main.style.display="block";
 
-    main.style.width = (screen.width-sidenav.offsetWidth).toString() + "px";
-    main.style.height = (screen.height - topnav.offsetHeight).toString() + "px";
-    main.style.marginLeft = (sidenav.offsetWidth-8).toString() + "px";
-    main.style.marginTop = (topnav.offsetHeight).toString() + "px";
+    // main.style.width = (screen.width-sidenav.offsetWidth).toString() + "px";
+    // main.style.height = (screen.height - topnav.offsetHeight).toString() + "px";
+    // main.style.marginLeft = (sidenav.offsetWidth-8).toString() + "px";
+    // main.style.marginTop = (topnav.offsetHeight).toString() + "px";
     // if (id==="litsum"){
     //     myFunc(id+"2");
     // }
 }
 
-main.onload
-{
-    /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-    var dropdown = document.getElementsByClassName("dropdown-btn");
-    var i;
-    for (i = 0; i < dropdown.length; i++) {
-        dropdown[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-        topnav.style.marginLeft = sidenav.offsetWidth.toString() + "px";
-        main.style.marginLeft = sidenav.offsetWidth.toString() + "px";
-        } else {
-        dropdownContent.style.display = "block";
-        topnav.style.marginLeft = dropdownContent.offsetWidth.toString() + "px";
-        main.style.marginLeft = dropdownContent.offsetWidth.toString() + "px";
-        }
-        });
-    }
-    resize()
-    // window.addEventListener('resize',resize);
-    // window.onresize=resize;
-
-}
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+    // topnav.style.marginLeft = sidenav.offsetWidth.toString() + "px";
+    // main.style.marginLeft = sidenav.offsetWidth.toString() + "px";
+    } else {
+    dropdownContent.style.display = "block";
+    };
+    });
+};
+resize()
